a87647f23737eb53f97d9ff7d1707dd765654766
4020405 <mostafa.abedini.back@gmail.com>
2026-05-10 11:37:36 +0330

    update : fix PWA

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+
     tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'اربعین تی وی',
+        short_name: 'ArbaeenTV',
+        description: 'تلویزیون اینترنتی جبهه مقاومت',
+
+        theme_color: '#000000',
+        background_color: '#ffffff',
+
+        display: 'standalone',
+
+        start_url: '/',
+
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
+
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 3008,
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://panel.makeenacademy.ir',
-    //     changeOrigin: true,
-    //     secure: false,
-    //     rewrite: (path) => path.replace(/^\/api/, '/api')
-    //   }
-    // }
-  }
-  // base: 'http://bk.arbaeentv.com/api/',
+  },
 })
