8f8a5877719a3aa398f1954cf16acb19adbe9855
4020405 <mostafa.abedini.back@gmail.com>
2026-06-08 10:54:03 +0330

    update:fix pwa

--- vite.config.js
+++ vite.config.js
@@ -11,29 +11,30 @@ export default defineConfig({
 
     VitePWA({
       registerType: 'autoUpdate',
-
+      includeAssets: ['icons/*.png', 'vite.svg'],
+      devOptions: {
+        enabled: true
+      },
       manifest: {
         name: 'اربعین تی وی',
         short_name: 'ArbaeenTV',
         description: 'تلویزیون اینترنتی جبهه مقاومت',
-
         theme_color: '#000000',
         background_color: '#ffffff',
-
         display: 'standalone',
-
         start_url: '/',
-
         icons: [
           {
-            src: '/icons/icon-192.png',
+            src: 'icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            src: '/icons/icon-512.png',
+            src: 'icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any maskable',
           },
         ],
       },
